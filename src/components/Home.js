3ff6afeb93e6d8d329fb54ad58fc7eefb3eb6518
syedbaji8 <syedbaji8@gmail.com>
2025-07-08 17:24:37 +0530

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = (props) => {
     return isLoading?<Loading setisLoading={setisLoading} />:(
         <div className="col-lg-12 p-0 d-flex flex-row flex-wrap align-items-center justify-content-center h-100 to-top align-content-center">
             <div className="col-lg-12 p-0 mx-auto profile-pic-block mb-3 bs-1 d-block d-lg-none">
-                <img className="img-fluid w-100" src={process.env.PUBLIC_URL + '/images/pro-1.JPG'} alt="logo" />
+                <img className="img-fluid w-100" src={process.env.PUBLIC_URL + '/images/samiulla.jpg'} alt="logo" />
             </div>
             <div className="col-lg-12 p-0 text-center">
                 <h4 className="text-center">Hello! I am</h4>
