4e0bb94293f7bed1520bb4c965d74f0d348c55b1
Syed Shaik <syedshaik@Admins-MacBook-Pro.local>
2025-07-08 16:29:13 +0530

    Updates

---
+++ precache-manifest.022d881741edf1aba1b478dbb17190c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248f3d98006f1551ee622b496dba9052",
+    "url": "/sami-profile/index.html"
+  },
+  {
+    "revision": "73bc66884632dcb50961",
+    "url": "/sami-profile/static/css/2.17ba3353.chunk.css"
+  },
+  {
+    "revision": "cb10705ced180213b191",
+    "url": "/sami-profile/static/css/main.cddb93fd.chunk.css"
+  },
+  {
+    "revision": "73bc66884632dcb50961",
+    "url": "/sami-profile/static/js/2.e117a07f.chunk.js"
+  },
+  {
+    "revision": "413b595604c5d3d1d7baf18f18130db2",
+    "url": "/sami-profile/static/js/2.e117a07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb10705ced180213b191",
+    "url": "/sami-profile/static/js/main.745c40bb.chunk.js"
+  },
+  {
+    "revision": "964be464c2d2fc65b051",
+    "url": "/sami-profile/static/js/runtime-main.9fb5cef1.js"
+  }
+]);
